acd697bd43ca841f3e389ee9301a1410bdff779d
kuroko <hkuroko@qq.com>
2022-01-19 09:52:47 +0800

    Fix:自动登录失败没有关闭bot

--- src/main/kotlin/com/hkuroko/mirai/AutoLoginFailedException.kt
+++ src/main/kotlin/com/hkuroko/mirai/AutoLoginFailedException.kt
@@ -3,6 +3,6 @@ package com.hkuroko.mirai
 import net.mamoe.mirai.network.CustomLoginFailedException
 
 class AutoLoginFailedException(
-    killBot: Boolean = false,
+    killBot: Boolean = true,
     message: String? = "Auto login failed,Please configure device-json in application.yml"
 ) : CustomLoginFailedException(killBot, message)
